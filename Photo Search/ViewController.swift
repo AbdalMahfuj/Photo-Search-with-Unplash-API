d44fcc701091dfd4a71a5c5b93ffe96c3a046d2e
Abdullah Al Mahfug <54243174+AbdalMahfuj@users.noreply.github.com>
2023-04-26 14:09:51 +0600

    function separated with extension

--- Photo Search/ViewController.swift
+++ Photo Search/ViewController.swift
@@ -7,18 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UISearchBarDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var myCollectionView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
     
-    var results: [Result] = []
+    private var results: [Result] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    
+    private func setupUI() {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
-        layout.itemSize = CGSize(width: view.frame.size.width/2, height: view.frame.size.width/2)
+        layout.itemSize = CGSize(width: view.frame.size.width/2, height: view.frame.size.width/2) // uiscreen
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 0
         myCollectionView.register(UINib(nibName: "ImageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
@@ -28,44 +33,13 @@ class ViewController: UIViewController, UICollectionViewDataSource, UISearchBarD
         myCollectionView.backgroundColor = .systemBackground
     }
     
+    
+    
+    
+}
 
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) { // default searchbar delefate function
-        searchBar.resignFirstResponder()
-        if let text = searchBar.text {
-            results = [] // before search results array should be empty
-            myCollectionView.reloadData()
-            fetchPhoto(text) // for every seach, fethphoto is called
-        }
-    }
+extension ViewController : UICollectionViewDataSource {
     
-    
-    func fetchPhoto(_ query: String) {
-        let urlString =
-        "https://api.unsplash.com/search/photos?page=30&query=\(query)&client_id=YRtnAmFdt80Jmcj-ZNIEBSkT8u4F9gwUMgJOpfhEXuY"
-        
-        guard let url = URL(string: urlString) else {
-            return
-        }
-        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
-            guard let data=data, error == nil else {
-                return
-            }
-            
-            do {
-                let jsonResults = try JSONDecoder().decode(APIResponse.self, from: data)
-                self?.results = jsonResults.results
-                print(self!.results.count)
-            }
-            catch {
-                print(error)
-            }
-            print("task ok")
-        }
-        
-        task.resume()
-    }
-    
-
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         results.count
     }
@@ -78,7 +52,51 @@ class ViewController: UIViewController, UICollectionViewDataSource, UISearchBarD
         //cell.backgroundColor = .red
         return cell
     }
+}
+
+
+extension ViewController: UISearchBarDelegate {
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if let text = searchBar.text {
+            results = []
+            myCollectionView.reloadData()
+            fetchPhoto(with: text)
+        }
+    }
     
 }
 
+
+extension ViewController {
+    
+    private func fetchPhoto(with query: String) {
+        let urlString =
+            "https://api.unsplash.com/search/photos?page=1&per_page=30&query=\(query)&client_id=YRtnAmFdt80Jmcj-ZNIEBSkT8u4F9gwUMgJOpfhEXuY"
+        
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data=data, error == nil else {
+                return
+            }
+            
+            do {
+                let jsonResults = try JSONDecoder().decode(APIResponse.self, from: data)
+                self?.results = jsonResults.results
+                DispatchQueue.main.async {
+                    self?.myCollectionView.reloadData()
+                    print(self!.results.count)
+                }
+            }
+            catch {
+                print(error)
+            }
+            print("task ok")
+        }
+        
+        task.resume()
+    }
+}
